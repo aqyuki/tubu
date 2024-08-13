d8f896a3427f3a664628ca25820514660ede8348
aqyuki <aqyuki@aqyuki.dev>
2024-08-13 10:01:36 +0900

    feat(bot): suport application command

---
+++ packages/platform/discord/command.go
@@ -0,0 +1,69 @@
+package discord
+
+import (
+	"context"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+type InteractionCreateHandler func(context.Context, *discordgo.Session, *discordgo.InteractionCreate)
+
+type Command interface {
+	Command() *discordgo.ApplicationCommand
+	Handler() InteractionCreateHandler
+}
+
+type CommandRouter struct {
+	commands    map[string]Command
+	contextFunc func() context.Context
+}
+
+type CommandRouterOption func(*CommandRouter)
+
+func WithCommand(command Command) CommandRouterOption {
+	return func(router *CommandRouter) {
+		name := command.Command().Name
+		if _, ok := router.commands[name]; ok {
+			panic("tried to register a command, but the same name command already exists")
+		}
+		router.commands[name] = command
+	}
+}
+
+func WithCommandContextFunc(f func() context.Context) CommandRouterOption {
+	return func(router *CommandRouter) {
+		router.contextFunc = f
+	}
+}
+
+func NewCommandRouter(opts ...CommandRouterOption) *CommandRouter {
+	router := &CommandRouter{
+		commands:    make(map[string]Command),
+		contextFunc: func() context.Context { return context.Background() },
+	}
+	for _, opt := range opts {
+		opt(router)
+	}
+	return router
+}
+
+func (r *CommandRouter) HandleInteractionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	// if the interaction is not a command, ignore it
+	if i.Type != discordgo.InteractionApplicationCommand {
+		return
+	}
+	command, ok := r.commands[i.ApplicationCommandData().Name]
+	if !ok {
+		return
+	}
+	command.Handler()(r.contextFunc(), s, i)
+}
+
+func (r *CommandRouter) Commands() []*discordgo.ApplicationCommand {
+	// TODO: change to use map.Values() when Go 1.23 is released
+	commands := make([]*discordgo.ApplicationCommand, 0, len(r.commands))
+	for _, command := range r.commands {
+		commands = append(commands, command.Command())
+	}
+	return commands
+}
