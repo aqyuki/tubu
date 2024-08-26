b62ab30541776113c00b1d75d94ff2f0390d5146
aqyuki <aqyuki@aqyuki.dev>
2024-08-26 21:33:14 +0900

    test: `ParseRedisConfig`関数の正常系のテストを追加

--- internal/setup/setup_test.go
+++ internal/setup/setup_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/aqyuki/tubu/packages/infra/redis"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -42,6 +43,62 @@ func TestParseBotConfig(t *testing.T) {
 			config, err := ParseBotConfig()
 			assert.Error(t, err, "expected error but received nil")
 			assert.Nil(t, config, "expected nil but received %v", config)
+		})
+	})
+}
+
+func TestParseRedisConfig(t *testing.T) {
+	t.Run("正常系", func(t *testing.T) {
+		t.Run("全ての環境変数がｓ指定されている場合", func(t *testing.T) {
+			t.Setenv("TUBU_REDIS_ADDRESS", "localhost:6379")
+			t.Setenv("TUBU_REDIS_PASSWORD", "password")
+			t.Setenv("TUBU_REDIS_DB", "1")
+			t.Setenv("TUBU_REDIS_POOL_SIZE", "20")
+
+			except := &redis.RedisConfig{
+				Address:  "localhost:6379",
+				Password: "password",
+				DB:       1,
+				PoolSize: 20,
+			}
+
+			config, err := ParseRedisConfig()
+			assert.NoError(t, err, "expected no error but received %v", err)
+			assert.Equal(t, except, config, "expected %v but received %v", except, config)
+		})
+
+		t.Run("TUBU_REDIS_DBが指定されていない場合，REDIS_DBにはデフォルト値が使用される", func(t *testing.T) {
+			t.Setenv("TUBU_REDIS_ADDRESS", "localhost:6379")
+			t.Setenv("TUBU_REDIS_PASSWORD", "password")
+			t.Setenv("TUBU_REDIS_POOL_SIZE", "20")
+
+			except := &redis.RedisConfig{
+				Address:  "localhost:6379",
+				Password: "password",
+				DB:       0,
+				PoolSize: 20,
+			}
+
+			config, err := ParseRedisConfig()
+			assert.NoError(t, err, "expected no error but received %v", err)
+			assert.Equal(t, except, config, "expected %v but received %v", except, config)
+		})
+
+		t.Run("TUBU_REDIS_POOL_SIZEが指定されていない場合，REDIS_POOL_SIZEにはデフォルト値が使用される", func(t *testing.T) {
+			t.Setenv("TUBU_REDIS_ADDRESS", "localhost:6379")
+			t.Setenv("TUBU_REDIS_PASSWORD", "password")
+			t.Setenv("TUBU_REDIS_DB", "1")
+
+			except := &redis.RedisConfig{
+				Address:  "localhost:6379",
+				Password: "password",
+				DB:       1,
+				PoolSize: 10,
+			}
+
+			config, err := ParseRedisConfig()
+			assert.NoError(t, err, "expected no error but received %v", err)
+			assert.Equal(t, except, config, "expected %v but received %v", except, config)
 		})
 	})
 }
