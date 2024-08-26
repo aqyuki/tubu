d4c13f32874d6899d6be2cb70589385accab302b
aqyuki <aqyuki@aqyuki.dev>
2024-08-26 18:40:25 +0900

    feat(cache): add support for redis

--- packages/cache/cache.go
+++ packages/cache/cache.go
@@ -3,9 +3,11 @@ package cache
 import (
 	"context"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/patrickmn/go-cache"
+	"github.com/redis/go-redis/v9"
 	"github.com/samber/lo"
 )
 
@@ -41,6 +43,25 @@ func (s *InMemoryCacheStore[T]) Get(_ context.Context, key string) (*T, error) {
 	v, ok := s.cache.Get(key)
 	if !ok {
 		return nil, ErrNotFound
+	}
+	return lo.ToPtr(v.(T)), nil
+}
+
+var _ CacheStore[string] = (*RedisCacheStore[string])(nil)
+
+type RedisCacheStore[T any] struct {
+	expiration time.Duration
+	client     *redis.Client
+}
+
+func (s *RedisCacheStore[T]) Set(ctx context.Context, key string, value T) error {
+	return s.client.Set(ctx, key, value, s.expiration).Err()
+}
+
+func (s *RedisCacheStore[T]) Get(ctx context.Context, key string) (*T, error) {
+	v, err := s.client.Do(ctx, key).Result()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get the value: %w", err)
 	}
 	return lo.ToPtr(v.(T)), nil
 }
