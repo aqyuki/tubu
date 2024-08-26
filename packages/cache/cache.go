d86c92dd8a0470f19826e67d2bb3305ae5cc1fd1
aqyuki <aqyuki@aqyuki.dev>
2024-08-26 18:29:09 +0900

    feat(cache): implements interface

--- packages/cache/cache.go
+++ packages/cache/cache.go
@@ -2,16 +2,21 @@ package cache
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/patrickmn/go-cache"
 	"github.com/samber/lo"
 )
 
+var ErrNotFound = errors.New("cache: key not found")
+
 type CacheStore[T any] interface {
-	Set(ctx context.Context, key, value string) error
+	Set(ctx context.Context, key string, value T) error
 	Get(ctx context.Context, key string) (*T, error)
 }
+
+var _ CacheStore[string] = (*InMemoryCacheStore[string])(nil)
 
 type InMemoryCacheStore[T any] struct {
 	cache             *cache.Cache
@@ -27,14 +32,15 @@ func NewInMemoryCacheStore[T any](exp, cleanup time.Duration) *InMemoryCacheStor
 	}
 }
 
-func (s *InMemoryCacheStore[T]) Set(key string, value T) {
+func (s *InMemoryCacheStore[T]) Set(_ context.Context, key string, value T) error {
 	s.cache.Set(key, value, s.defaultExpiration)
+	return nil
 }
 
-func (s *InMemoryCacheStore[T]) Get(key string) (*T, bool) {
+func (s *InMemoryCacheStore[T]) Get(_ context.Context, key string) (*T, error) {
 	v, ok := s.cache.Get(key)
 	if !ok {
-		return nil, false
+		return nil, ErrNotFound
 	}
-	return lo.ToPtr(v.(T)), true
+	return lo.ToPtr(v.(T)), nil
 }
