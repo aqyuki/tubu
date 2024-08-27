4fb1c19d0dbcd3b26d7e6742219363391be4b9bc
aqyuki <aqyuki@aqyuki.dev>
2024-08-27 09:10:17 +0900

    feat(cmd): 設定を環境変数以外からでも読み込めるように変更 (#64)

--- internal/setup/setup.go
+++ internal/setup/setup.go
@@ -1,44 +1,26 @@
 package setup
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/aqyuki/tubu/packages/cache"
+	"github.com/aqyuki/tubu/packages/config"
 	"github.com/aqyuki/tubu/packages/infra/redis"
-	"github.com/caarlos0/env/v11"
 )
 
 const (
-	environmentPrefix = "TUBU_"
 	defaultExpiration = 30 * time.Minute
 	defaultCleanup    = 30 * time.Minute
 )
 
-type Config struct {
-	Token      string        `env:"DISCORD_TOKEN,required"`
-	APITimeout time.Duration `env:"API_TIMEOUT" envDefault:"10s"`
-}
-
-func ParseBotConfig() (*Config, error) {
-	cfg, err := env.ParseAsWithOptions[Config](env.Options{Prefix: environmentPrefix})
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse bot config: %w", err)
-	}
-	return &cfg, nil
-}
-
-func ParseRedisConfig() (*redis.RedisConfig, error) {
-	cfg, err := env.ParseAsWithOptions[redis.RedisConfig](env.Options{Prefix: environmentPrefix})
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse redis config: %w", err)
-	}
-	return &cfg, nil
-}
-
-func SetupCacheStore[T any](cfg *redis.RedisConfig) cache.CacheStore[T] {
-	if cfg.IsEnabled() {
-		client, err := redis.NewRedisClient(cfg)
+func NewCacheStore[T any](cnf *config.Config) cache.CacheStore[T] {
+	if cnf.RedisEnabled {
+		client, err := redis.NewRedisClient(&redis.RedisConfig{
+			Address:  cnf.RedisAddr,
+			Password: cnf.RedisPassword,
+			DB:       cnf.RedisDB,
+			PoolSize: cnf.RedisPoolSize,
+		})
 		if err == nil {
 			return cache.NewRedisCacheStore[T](client, defaultExpiration)
 		}
